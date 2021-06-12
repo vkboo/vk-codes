8f90174475df3398ff84349b706f3c2d2861e973
tyounami <tyounami@163.com>
2021-06-12 22:04:52 +0800

    feat: vk-codes react-prompt 完成

--- react-prompt/README.md
+++ react-prompt/README.md
@@ -1,1 +1,6 @@
 # react-prompt
+## React编程式组件的编写与使用
+> 一般组件的用法都是引入组件后，挂载到render函数中的返回值中进行使用。但是，有部分组件如`Toast` `Alert` `Prompt` `ImagePreview`等等，组件功能比较单一，不会声明插槽改动内部的结构，针对这种组件，通常使用编程式UI的方式进行调用。
+
+### 使用方式： 先引入方法，再进行调用 
+查看`react-prompt/src/components/Prompt.jsx`源代码
--- react-prompt/src/components/Prompt.jsx
+++ react-prompt/src/components/Prompt.jsx
@@ -1,17 +1,11 @@
 import ReactDOM from 'react-dom';
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './style.module.css';
 const Prompt = ({
     message,
     onOk = () => { },
     onCancel = () => { },
 }) => {
-    useEffect(() => {
-        console.log('挂载');
-        return () => {
-            console.log('卸载')
-        }
-    }, []);
     return (
         <div className={styles.container}>
             <h3>{message}</h3>
@@ -22,15 +16,11 @@ const Prompt = ({
 };
 
 export const prompt = (message, { onOk, onCancel }) => {
-    console.warn('123')
     const div = document.createElement('div');
-    div.className = 'vk-prompt';
     document.body.appendChild(div);
     const destroy = () => {
         const unmountResult = ReactDOM.unmountComponentAtNode(div);
-        if (unmountResult) {
-            div && div.remove();
-        }
+        unmountResult && div && div.remove();
     };
     const props = {
         message,
